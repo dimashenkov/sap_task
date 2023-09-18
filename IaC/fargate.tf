3c2ddb31dda7bd76bc6eb38c4fa655e47e96f5bf
Dimitar Shenkov <dimitar.shenkov@mansion.gom>
2023-09-18 13:07:21 +0300

    end point

--- IaC/fargate.tf
+++ IaC/fargate.tf
@@ -154,6 +154,13 @@ resource "aws_security_group" "microblog-allow" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "allow tcp access from microblog container"
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
 }
 
 resource "aws_ecs_service" "microblog" {
